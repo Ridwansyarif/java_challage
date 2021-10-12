463937a4719b6ac864dcfe5db359c217448ea8aa
Dvnhrdt <harditiadovan@gmail.com>
2021-10-13 02:34:24 +0700

    first commit

---
+++ biodataridwan.java
@@ -0,0 +1,20 @@
+public class biodataridwan {
+    public static void main(String args []) {
+        String firstName = "Ridwan";
+        String lastName = "Syarif";
+        String address = "Bukittinggi";
+        int age = 20;
+        String tahun = " Tahun";
+        String phoneNumber = "085787883456";
+
+        String hobi = "Bermain Game";
+        String makananFav = "Bakso";
+        System.out.println("First name   : " + firstName + lastName);
+        System.out.println("Address      : " + address);
+        System.out.println("Age          : " + age + tahun);
+        System.out.println("Phone Number : " + phoneNumber);
+        System.out.println("Hobi         : " + hobi);
+        System.out.println("Makanan Fav  : " + makananFav);
+
+    }
+}
